e804cf1543ca58bc60db89ef146f556b097a4c14
PANAMA-TEC <56969999+PANAMA-TEC@users.noreply.github.com>
2022-04-17 14:33:58 -0500

    AGREGANDO LOS ARCHIVOS INICIALES

---
+++ src/componentes/contenedores/ListaContactos/ListaContactos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Contacto } from '../../../clases/contacto.class';
+import Contactos from '../../simples/Contactos/contactos';
+import './ListaContactos.css';
+
+const Listacontactos = () => {
+    
+    const DEFAULT_CONTACT = new Contacto ("Gabriel", "Garcia", "gagarcia@panama-tec.com", false);
+    
+    return (
+        
+        <div className='lista-contactos'>
+            <h1 className='titulo-contactos'> LISTA DE CONTACTOS</h1>
+            <Contactos contacto={DEFAULT_CONTACT}></Contactos>
+        </div>
+    );
+}
+
+export default Listacontactos;
---
+++ src/componentes/simples/Contactos/contactos.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Contacto } from '../../../clases/contacto.class';
+import './contactos.css'
+
+const Contactos = ({contacto}) => {
+    
+    return (
+        <div className='contacto'>
+            <h2>
+                Nombre: {contacto.nombre } 
+            </h2>
+            <h2>
+                Apellido: {contacto.apellido }
+            </h2>
+            <h2>
+                Email: {contacto.email }
+            </h2>
+            <h2>
+                Estado: {contacto.conectado ? "EN LINEA":"FUERA DE LINEA" }
+            </h2>
+        </div>
+    );
+};
+
+
+Contactos.propTypes = {
+    contacto: PropTypes.instanceOf(Contacto)
+
+};
+
+
+export default Contactos;
